456931f41f0d4a89452d74d6fc2cc6f7bd66974e
Andrés G. Castillo <52080418+andres2313@users.noreply.github.com>
2020-07-28 13:16:02 -0500

    Add files via upload

---
+++ ProyectoFinal-TopicosEspecialesII - AndresGonzalez_AlexanderGonzalez/flappy_bird_textured.py
@@ -0,0 +1,93 @@
+import pygame, sys, time, random
+from pygame.locals import *
+
+pygame.init()
+width = 500
+height = 700
+play_surface = pygame.display.set_mode((width, height))
+background_image = pygame.image.load("back.png").convert()
+bird_image = pygame.image.load("bird.png").convert_alpha()
+top_pipe = pygame.image.load("pipe_top.png").convert_alpha()
+bot_pipe = pygame.image.load("pipe_bot.png").convert_alpha()
+fps = pygame.time.Clock()
+
+#funcion marcador
+def draw_text(surface, text, size, x, y):
+    font = pygame.font.SysFont("serif", size)
+    text_surface = font.render(text, True, (255, 255, 255))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surface.blit(text_surface, text_rect)
+  
+#top pipe
+
+def pipe_random_height():
+    pipe_h = [random.randint(200,(height/2)-20), random.randint((height/2)+20, height-200)]
+    return pipe_h
+
+def main():
+    score = 0
+    player_pos = [100, 350]
+    gravity = 1
+    speed = 0
+    jump = -30
+
+    #pipe
+    pipe_pos = 700
+    pipe_width = 50
+    pipe_height = pipe_random_height()
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        speed += jump
+
+        #Down Force
+        speed += gravity
+        speed *= 0.95
+        player_pos[1] += speed
+
+        #pipe
+        if pipe_pos >= -20:
+            pipe_pos -= 10
+        else:
+            pipe_pos = 700
+            pipe_height = pipe_random_height()
+            score += 1
+
+        #Surface
+        play_surface.blit(background_image, [0, 0])
+
+        #drawpipe
+        play_surface.blit(top_pipe, (pipe_pos, -pipe_height[0]))
+        play_surface.blit(bot_pipe, (pipe_pos, pipe_height[1]))
+
+        #player
+        play_surface.blit(bird_image, (int(player_pos[0]), int(player_pos[1])))
+
+        #Collision
+        if player_pos[1] <= (-pipe_height[0]+500) or player_pos[1] >= pipe_height[1]:
+            if player_pos[0] in list(range(pipe_pos, pipe_pos+pipe_width)):
+                print(f"Game Over. Score: {score}")
+
+        #Borders
+        if player_pos[1] >= height:
+            player_pos[1] = height
+            speed = 0
+        elif player_pos[1] <= 0:
+            player_pos[1] = 0
+            speed = 0
+
+        pygame.display.flip()
+        fps.tick(25)
+        
+        #On screen score
+        draw_text(play_surface, str(score),25, width // 2, 10)  
+
+main()
+pygame.quit()
